db7b2bf75371c3ccc6fd4605fbf3ea4dff496cad
grumpycoder <marklawrence72@gmail.com>
2015-05-18 22:18:05 -0500

    agency theme and index page

---
+++ SpaDay.web/app/app.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    "use strict";
+
+    function configRoutes($routeProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/home/home.html'
+            })
+            .when('/home', {
+                templateUrl: 'app/home/home.html'
+            })
+            .when('/about', {
+                templateUrl: 'app/home/about.html'
+            })
+            .when('/services', {
+                templateUrl: 'app/home/services.html'
+            })
+            .when('/schedule', {
+                templateUrl: 'app/schedule/schedule.html',
+                controller: 'ScheduleCtrl',
+                controllerAs: 'vm'
+            });
+
+        $routeProvider.otherwise('/');
+    }
+
+    angular.module('spaApp', [
+        'ngRoute',
+        'ui.bootstrap'
+    ]).config(['$routeProvider', configRoutes]);
+}());
+
+
+
